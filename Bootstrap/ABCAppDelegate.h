3125de5d154bd5fc00d8999528d0f657aac1d87e
Joe Masilotti <joseph.masilotti@gmail.com>
2013-08-01 18:58:47 -0400

    Initial Commit

---
+++ Bootstrap/ABCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ABCAppDelegate.h
+//  Bootstrap
+//
+//  Created by Joseph Masilotti on 8/1/13.
+//  Copyright (c) 2013 Example.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Bootstrap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Bootstrap
+//
+//  Created by Joseph Masilotti on 8/1/13.
+//  Copyright (c) 2013 Example.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ABCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ABCAppDelegate class]));
+    }
+}
